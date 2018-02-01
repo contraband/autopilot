4c9ea1d917e67dbfb850a5f6dcc690a880729e39
Christopher Brown <cbrown@pivotal.io>
2018-02-01 12:04:11 -0800

    build: specify the kind of binaries we want
    Specifically, 64-bit and staticly-compiled is the sweet-spot.

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -5,13 +5,13 @@ set -e
 SANDBOX=$(mktemp -d)
 
 echo "building linux..."
-GOOS=linux go build -o $SANDBOX/autopilot-linux github.com/contraband/autopilot
+CGO_ENABLED=0 GOARCH=amd64 GOOS=linux go build -o $SANDBOX/autopilot-linux github.com/contraband/autopilot
 
 echo "building os x..."
-GOOS=darwin go build -o $SANDBOX/autopilot-darwin github.com/contraband/autopilot
+CGO_ENABLED=0 GOARCH=amd64 GOOS=darwin go build -o $SANDBOX/autopilot-darwin github.com/contraband/autopilot
 
 echo "building windows..."
-GOOS=windows go build -o $SANDBOX/autopilot.exe github.com/contraband/autopilot
+CGO_ENABLED=0 GOARCH=amd64 GOOS=windows go build -o $SANDBOX/autopilot.exe github.com/contraband/autopilot
 
 echo
 
