5e34f165d172fd1253070c6c7ada984a03cb447e
Christopher Brown <cbrown@pivotal.io>
2018-09-05 17:20:30 +0000

    bump to 0.0.8

--- autopilot.go
+++ autopilot.go
@@ -162,7 +162,7 @@ func (AutopilotPlugin) GetMetadata() plugin.PluginMetadata {
 		Version: plugin.VersionType{
 			Major: 0,
 			Minor: 0,
-			Build: 7,
+			Build: 8,
 		},
 		Commands: []plugin.Command{
 			{
